ff1f568ff609374916f5753fe89d0c08b448db9f
Asami Takaoka <asamiyamabe61@gmail.com>
2024-05-12 22:54:19 +0900

    投稿記事のカテゴリを追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
       resources :baby_foods
       #カテゴリー分けに関係するところのみ記載
       get 'posts/house_work' => 'posts#house_work'
+      get 'posts/work' => 'posts#work'
       get 'posts/money' => 'posts#money'
+      get 'posts/human_relations' => 'posts#human_relations'
+      get 'posts/outing_with_baby' => 'posts#outing_with_baby'
+      get 'posts/health' => 'posts#health'
+      get 'posts/developmental' => 'posts#developmental'
       get 'posts/baby_food' => 'posts#baby_food'
       get 'posts/childbirth' => 'posts#childbirth'
       get 'posts/breastfeeding' => 'posts#breastfeeding'
