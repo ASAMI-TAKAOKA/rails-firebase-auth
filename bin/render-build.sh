b3b403b71fbf01e8515845f34c9745a7d4dbd0fe
Asami Takaoka <asamiyamabe61@gmail.com>
2024-05-15 19:49:16 +0900

    本番環境のDBのデータを削除(やり直し)

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,7 +6,7 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 # bundle exec rake db:migrate
-# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset # デプロイの際にデータベースをリセット。 # 一度リセットしたらコメントアウトしてbundle exec rake db:migrateを元に戻すこと！
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset # デプロイの際にデータベースをリセット。 # 一度リセットしたらコメントアウトしてbundle exec rake db:migrateを元に戻すこと！
 # 参考:
 # https://qiita.com/hashioga2017/items/b69cb070c0073e9c26c7
 # https://memorandums.hatenablog.com/entry/2023/01/17/211750
