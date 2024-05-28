01616d994f41d66fc11c434d04482a356eeabdd2
Asami Takaoka <asamiyamabe61@gmail.com>
2024-05-28 23:05:33 +0900

    DBにseedデータ投入

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,9 +5,9 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-# bundle exec rake db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset # デプロイの際にデータベースをリセット。 # 一度リセットしたらコメントアウトしてbundle exec rake db:migrateを元に戻すこと！
-# bundle exec rails db:seed
+bundle exec rake db:migrate
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset # デプロイの際にデータベースをリセット。 # 一度リセットしたらコメントアウトしてbundle exec rake db:migrateとbundle exec rails db:seedを元に戻すこと！
+bundle exec rails db:seed
 # 参考:
 # https://qiita.com/hashioga2017/items/b69cb070c0073e9c26c7
 # https://memorandums.hatenablog.com/entry/2023/01/17/211750
